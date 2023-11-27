43b8283ccb4d4399bccc681830864254a0964146
Unblocxx <150937565+Unblocxx@users.noreply.github.com>
2023-11-27 12:25:18 -0600

    Update proxy.js bruh

--- api/proxy.js
+++ api/proxy.js
@@ -1,7 +1,7 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = (req, res) => {
-  let target = "https://www.roblox.com/";//your website url
+  let target = "https://www.coomer.party/";//your website url
   //   if (
   //     req.url.startsWith("/api") ||
   //     req.url.startsWith("/auth") ||
