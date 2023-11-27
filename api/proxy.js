0e9526ba51f221a486e5fbdfe1e7213fe9baabb9
Unblocxx <150937565+Unblocxx@users.noreply.github.com>
2023-11-27 12:17:39 -0600

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,7 +1,7 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = (req, res) => {
-  let target = "https://www.zestypies.click/";//your website url
+  let target = "https://www.proxium.com/";//your website url
   //   if (
   //     req.url.startsWith("/api") ||
   //     req.url.startsWith("/auth") ||
